192d7084220bcf8b05fa3e56612b5bc450af033e
FanFan_laptop <oceaneferg@gmail.com>
2025-11-12 17:00:55 +0800

    Change Hello to example.py

--- cogs/example.py
+++ cogs/example.py
@@ -2,12 +2,12 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-# 定義名為 Main 的 Cog
-class HelloBack(commands.Cog):
+# 定義名為 example 的 Cog
+class example(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # 關鍵字觸發
+    # 事件監聽
     @commands.Cog.listener()
     async def on_ready(self):
         await self.bot.tree.sync()
@@ -19,11 +19,11 @@ class HelloBack(commands.Cog):
         if message.content == "Hello":
             await message.channel.send("Hello, world!")
 
-    # 前綴指令
-    @app_commands.command(name="hello",description="send helloback to you.")
+    # Slash指令
+    @app_commands.command(name="example",description="send helloback to you.")
     async def hello(self, interaction: discord.Interaction):
         await interaction.response.send_message("Hello, world!")
 
 # Cog 載入 Bot 中
 async def setup(bot):
-    await bot.add_cog(HelloBack(bot))
+    await bot.add_cog(example(bot))
