a0ace016a10fcc53461d884c262742dc62d79bbd
FanFan_laptop <oceaneferg@gmail.com>
2026-04-15 17:13:44 +0800

    Update StructureRecommend.md

--- cogs/LifeTracker/src/__init__.py
+++ cogs/LifeTracker/src/__init__.py
@@ -1,6 +1,6 @@
-from .speech_to_text import stt_whisper
+#from .speech_to_text import stt_whisper
 from .chart_generator import generate_donut_chart
 __all__=[
-    "stt_whisper",
+    #"stt_whisper",
     "generate_donut_chart"
 ]
