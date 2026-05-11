8b2da23e02093fae52458089b5d88d4e4cdc2d95
FanFan_4070s <oceaneferg@gmail.com>
2026-05-11 22:31:26 +0800

    gmail

--- cogs/Gmail/utils/__init__.py
+++ cogs/Gmail/utils/__init__.py
@@ -1,5 +1,5 @@
-from .Gmail_manager import EmailDatabaseManager
-from .Gmail_tool import EmailTools
+from .mail_manager import EmailDatabaseManager
+from .mail_tool import EmailTools
 from .Gmail_AI_Analyzer import Gmail_AI_Analyzer
 __all__ = [
     'EmailDatabaseManager',
